2ee746049acefa484bf1593220a71ecc101f99a0
Sailing Zhang <2991394066@qq.com>
2022-06-07 01:46:01 +0800

    初始化

---
+++ src/main/java/com/sailing/interfacetestplatform/InterfaceTestPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sailing.interfacetestplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceTestPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterfaceTestPlatformApplication.class, args);
+    }
+
+}
